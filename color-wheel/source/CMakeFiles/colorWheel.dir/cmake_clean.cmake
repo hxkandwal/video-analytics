de28458a62ffa5fbc704feaf52e04cd9e7833bb4
Himanshu Kandwal <himanshuk.19@gmail.com>
2016-07-14 02:25:11 -0500

    representation of color wheel

---
+++ color-wheel/source/CMakeFiles/colorWheel.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/colorWheel.dir/colorWheel.cpp.o"
+  "colorWheel.pdb"
+  "colorWheel"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/colorWheel.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
